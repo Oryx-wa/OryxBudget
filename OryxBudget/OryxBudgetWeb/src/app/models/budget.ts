8cfd7d1cdee5a00f9f9b6cd3885855bc4792819e
sodiqlekan <olalekan.kelani@oryx-wa.com>
2017-05-09 18:59:42 +0100

    changes

---
+++ OryxBudget/OryxBudgetWeb/src/app/models/budget.ts
@@ -0,0 +1,10 @@
+export interface Budgets {
+    id: string;
+    totalBudgetAmount: number;
+    totalAmountLC: number;
+    totalAmountUSD: number;
+    actualBudgetAmount: number;
+    actualAmountLC: number;
+    actualAmountUSD: number;
+    description: string;
+}
--- OryxBudget/OryxBudgetWeb/src/app/operator-view/operator-view.module.ts
+++ OryxBudget/OryxBudgetWeb/src/app/operator-view/operator-view.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { OperatorViewComponent } from './operator-view.component';
-
+import {SharedModule} from './../shared/shared.module';
+import { SecurityService } from './../login/security.service';
+import { OperatorViewComponent} from './operator-view.component';
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
   ],
   declarations: [OperatorViewComponent],
-  exports: [OperatorViewComponent]
+   exports: [
+    OperatorViewComponent]
 })
+
 export class OperatorViewModule { }
