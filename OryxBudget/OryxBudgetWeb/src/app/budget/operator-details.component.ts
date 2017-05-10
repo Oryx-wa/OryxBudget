ee3456eab4c666989dabf0e501c15fdf3a22dee4
Omotayad <tayo.adegbola@oryx-wa.com>
2017-05-10 22:03:57 +0100

    Operator details Napims view

--- OryxBudget/OryxBudgetWeb/src/app/budget/operator-details.component.ts
+++ OryxBudget/OryxBudgetWeb/src/app/budget/operator-details.component.ts
@@ -3,8 +3,8 @@ import { Router, ActivatedRoute, Params } from '@angular/router';
 import { Http, URLSearchParams } from '@angular/http';
 import 'rxjs/add/operator/switchMap';
 import 'rxjs/add/operator/map';
-import { Budgets } from './../models/budget';
-import {Observable} from 'rxjs';
+import { Budgets, Operators } from './../models';
+import { Observable } from 'rxjs';
 import { SecurityService } from './../login/security.service';
 @Component({
   selector: 'app-operator-details',
@@ -13,6 +13,8 @@ import { SecurityService } from './../login/security.service';
 })
 export class OperatorDetailsComponent implements OnInit {
   budgets$: Observable<Budgets[]>;
+  operator$: Observable<Operators>;
+  operator: Operators;
   constructor(
     private route: ActivatedRoute,
     private router: Router,
@@ -28,17 +30,31 @@ export class OperatorDetailsComponent implements OnInit {
   }
 
   getOperator(id: string) {
-    const url = this.securityService.getUrl('Budget/GetByOperator');
-        let params: URLSearchParams = new URLSearchParams();
-        params.append('operatorId', id);
+    let url = this.securityService.getUrl('Budget/GetByOperator');
+    const params: URLSearchParams = new URLSearchParams();
+    params.append('operatorId', id);
 
-        this.budgets$ = this._http.get(url, {
-          headers: this.securityService.getHeaders(),
-          body: '',
-          params: params
-        }).map(res => res.json());
+    this.budgets$ = this._http.get(url, {
+      headers: this.securityService.getHeaders(),
+      body: '',
+      params: params
+    }).map(res => res.json());
 
-        this.budgets$.subscribe(b => console.log(b));
+    url = this.securityService.getUrl('Operator/GetById');
+    const params1: URLSearchParams = new URLSearchParams();
+    params1.append('id', id);
+
+    this.operator$ = this._http.get(url, {
+      headers: this.securityService.getHeaders(),
+      body: '',
+      params: params1
+    }).map(res => res.json());
+
+    this.operator$.subscribe(operator => this.operator = operator);
+
+
+    
+
   }
 
 }
