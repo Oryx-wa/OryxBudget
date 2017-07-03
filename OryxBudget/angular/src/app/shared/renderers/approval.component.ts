fcd27e0ff2d7b554f487320cf25bae2d058b5e55
Omotayad <tayo.adegbola@oryx-wa.com>
2017-07-03 14:52:00 +0100

    updated

--- OryxBudget/angular/src/app/shared/renderers/approval.component.ts
+++ OryxBudget/angular/src/app/shared/renderers/approval.component.ts
@@ -3,7 +3,17 @@ import {ICellRendererAngularComp} from 'ag-grid-angular/main';
 
 @Component({
     selector: 'app-approval',
-    template: ``
+    template: `<div class="row">
+                              <div class="">
+                                <select materialize="material_select" id="status" 
+                                name="status" (change)="operatorSelected($event.target.value)>										
+                                <option  value="" disabled selected ></option>	
+                    <option  [value]="accepted" >Accepted</option>          
+                    <option  [value]="rejected" >Rejected</option>                                
+                      </select>
+                               
+                              </div>
+                            </div>`
 })
 
 export class NameComponent implements ICellRendererAngularComp {
@@ -11,6 +21,10 @@ export class NameComponent implements ICellRendererAngularComp {
 
     agInit(params: any): void {
        
+    }
+
+     public invokeParentMethod(type: string) {
+        this.params.context.componentParent.methodFromParent(this.params.data.code, type );
     }
 
 }
