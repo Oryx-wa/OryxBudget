dc2ac631af79d132d7b89d4de9df3597dd26e7e5
sodiqlekan <olalekan.kelani@oryx-wa.com>
2017-07-04 10:04:32 +0100

    facilities

--- OryxBudget/angular/src/app/workprogram/facilities/facilities.component.ts
+++ OryxBudget/angular/src/app/workprogram/facilities/facilities.component.ts
@@ -1,15 +1,124 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, ChangeDetectionStrategy, OnDestroy } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, FormArray, Validators } from '@angular/forms';
+import { MaterializeDirective, MaterializeAction } from 'angular2-materialize';
+import { Observable } from 'rxjs/Observable';
+import { Store } from '@ngrx/store';
+import {
+  AppState, Budget, UserSelector, BudgetActions, Actual, ActualSelector,
+  BudgetLines, BudgetSelector, BudgetLineActions, BudgetLineSelector, ActualActions
+} from './../../redux';
+import { DisplayModeEnum } from '../../shared/shared-enum.enum';
 
 @Component({
   selector: 'app-facilities',
   templateUrl: './facilities.component.html',
-  styleUrls: ['./facilities.component.scss']
+  styleUrls: ['./facilities.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class FacilitiesComponent implements OnInit {
+export class FacilitiesComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  actions1 = new EventEmitter<string | MaterializeAction>();
+  public showApproval = false;
+  form: FormGroup;
+  budget$: Observable<Budget>;
+  lines$: Observable<BudgetLines[]>;
+  actuals$: Observable<Actual[]>
+  budgetId = '';
+
+  public napims$: Observable<boolean>;
+  public subCom$: Observable<boolean>;
+  public tecCom$: Observable<boolean>;
+  public malCom$: Observable<boolean>;
+  public display$: Observable<string>;
+
+  public dept$: Observable<string>;
+  public operator$: Observable<boolean>;
+  private alive = true;
+
+  public displayMode: DisplayModeEnum;
+  public displayModeEnum = DisplayModeEnum;
+  constructor(private fb: FormBuilder, private store: Store<AppState>) { }
 
   ngOnInit() {
+    this.displayMode = DisplayModeEnum.Card;
+    this.dept$ = this.store.select(UserSelector.dept);
+    this.subCom$ = this.store.select(UserSelector.subCom);
+    this.tecCom$ = this.store.select(UserSelector.tecCom);
+    this.malCom$ = this.store.select(UserSelector.malCom);
+    this.napims$ = this.store.select(UserSelector.napims);
+    this.display$ = this.store.select(UserSelector.display);
+
+    this.display$.subscribe(display => {
+      if (this.budgetId !== '' && this.budgetId !== null) {
+        switch (display) {
+          case 'budget':
+            this.changeDisplayMode(DisplayModeEnum.Budget);
+            this.store.dispatch(new BudgetLineActions.LoadItemsAction(this.budgetId));
+            break;
+          case 'actual':
+            this.changeDisplayMode(DisplayModeEnum.Actual);
+            this.store.dispatch(new ActualActions.LoadItemsAction(this.budgetId));
+            break;
+          case 'cashCall':
+            this.changeDisplayMode(DisplayModeEnum.CashCall);
+
+            break;
+          default:
+            this.changeDisplayMode(DisplayModeEnum.Budget);
+            break;
+        }
+      }
+
+    });
+    this.budget$ = this.store.select(BudgetSelector.selectedBudget);
+    this.budget$
+      .takeWhile(() => this.alive)
+      .subscribe(budget => {
+        if (budget.id) {
+          if (budget.id !== '' && budget.id !== null) {
+            this.budgetId = budget.id;
+            switch (this.displayMode) {
+              case DisplayModeEnum.Budget:
+                this.store.dispatch(new BudgetLineActions.LoadItemsAction(this.budgetId));
+                break;
+              case DisplayModeEnum.Actual:
+                this.store.dispatch(new ActualActions.LoadItemsAction(this.budgetId));
+                break;
+              default:
+                break;
+            }
+          }
+        }
+
+      });
+    this.form = this.fb.group({
+      status: new FormControl({}),
+    });
+    this.lines$ = this.store.select(BudgetLineSelector.getBudgetLineCollection);
+    this.actuals$ = this.store.select(ActualSelector.getActualCollection);
+
+    this.changeDisplayMode(DisplayModeEnum.Budget);
+  }
+  openFirst() {
+    this.showApproval = true;
+  }
+
+  getBudgetLines() {
+
+  }
+
+  changeDisplayMode(mode: DisplayModeEnum) {
+    // // console.log(mode); 
+    this.displayMode = mode;
+  }
+
+  selectBudget(id: string) {
+    this.store.dispatch(new BudgetActions.SelectItemAction(id));
+    // this.store.dispatch(new BudgetLineActions.LoadItemsAction(this.budgetId));
+  }
+
+  ngOnDestroy() {
+    this.alive = false;
   }
 
 }
--- OryxBudget/angular/src/app/workprogram/facilities/facilities.module.ts
+++ OryxBudget/angular/src/app/workprogram/facilities/facilities.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FacilitiesComponent } from './facilities.component';
-
+import { FacilitiesRoutingModule} from './facilities.routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from './../../shared/shared.module';
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    FacilitiesRoutingModule,
+    SharedModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
-  declarations: [FacilitiesComponent]
+  declarations: [FacilitiesComponent],
+   exports: [FacilitiesComponent]
 })
 export class FacilitiesModule { }
